72912426cb7938953bcf94327b20ddb0691c5c02
Benjamin Brachmann <benjamin.brachmann@icloud.com>
2023-03-02 03:37:13 +0100

    added express route testing

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
 import 'reflect-metadata'
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
-import { router } from './utils/decorators/controller'
-import { openApiMiddleware, openApiErrorMiddleware } from './utils/middlewares/openapi'
+import { createExpress } from './express'
 
 /* Environment variables */
 dotenv.config()
 
 /* Express */
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan('tiny'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(openApiMiddleware)
-app.use(openApiErrorMiddleware)
-app.use(router)
+createExpress(process.env.PORT)
 
 /* Controllers */
 import './controllers/product'
-
-/* Server */
-const port = process.env.PORT
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
