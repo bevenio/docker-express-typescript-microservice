3f56dcbed6d2896812bf85e9d3f9dfb7fd8b0349
Benjamin Brachmann <benjamin.brachmann@icloud.com>
2023-04-01 03:30:46 +0200

    added port to database connector

--- src/common/database/connector.ts
+++ src/common/database/connector.ts
@@ -12,7 +12,7 @@ const connections: DatabaseConnections = {
 }
 
 const connectMongoDB = async () => {
-  const mongoConnectionUri = `${Environment.DB_MONGO_ADRESS}`
+  const mongoConnectionUri = `${Environment.DB_MONGO_ADRESS}:${Environment.DB_MONGO_PORT}`
   const mongoClientConnection = await MongoClient.connect(mongoConnectionUri)
   connections.mongo = mongoClientConnection.db(Environment.DB_MONGO_NAME)
   Logger.log(`Connected to mongodb [${mongoConnectionUri}/${Environment.DB_MONGO_NAME}]`)
